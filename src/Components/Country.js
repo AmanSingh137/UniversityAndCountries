6071c4a37f8d9726453a62fbd2e8dee96e4a1dc4
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2021-12-24 01:36:57 +0530

    data sent from main page to country page

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -1,12 +1,15 @@
 import React, { useState, useMemo, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Select from "react-select";
 import countryList from "react-select-country-list";
 import axios from 'axios';
 import ListGroup from 'react-bootstrap/ListGroup'
-import Describe from "./Describe";
+//import Describe from "./Describe";
 import './Country.css';
 
+
 const Country = () => {
+    let navigate = useNavigate();
     const [value, setValue] = useState("");
     let nothing = "Select a country";
     const options = useMemo(() => countryList().getData(), []);
@@ -16,14 +19,14 @@ const Country = () => {
     };
     const url = "http://universities.hipolabs.com/search?country="
     const [data, setData] = useState(null);
-    const [country, setCountry] = useState([]);
-    const [val, setVal] = useState(-1);
+    //const [country, setCountry] = useState([]);
+    //const [val, setVal] = useState(-1);
     // const changeHandler1 = () =>{
     //     setVal()
     // }
-    const clickHandler = (e) => {
-        console.log(e.target.value);
-    }
+    // const clickHandler = (e) => {
+    //     console.log(e.target.value);
+    // }
     useEffect(() => {
         const getData = async () => {
             try {
@@ -32,18 +35,20 @@ const Country = () => {
                     res = await axios.get(url + value.label);
                 // console.log(res.data[0].name);
                 const values = await res.data;
+                console.log(values);
                 //setCountry(values);
                 const displayData = await values.map((item, index) => {
+                    //console.log(item.web_pages);
                     return <>
                         <ListGroup>
-                            <ListGroup.Item variant="primary" className="mt-2" onClick={clickHandler}>{item.name}</ListGroup.Item>
+                            <ListGroup.Item variant="primary" className="mt-2" onClick={() => navigate('/country', { state: { name: item.name, web: item.web_pages, ct: value.label }, replace: true })}>{item.name}</ListGroup.Item>
                         </ListGroup>
                     </>
                 });
                 setData(displayData);
                 //console.log(displayData);
             } catch (err) {
-                console.log(err);
+                //console.log(err.res.status]);
             }
         }
         getData();
@@ -60,7 +65,7 @@ const Country = () => {
                 <Select options={options} value={value} onChange={changeHandler} />
                 {data}
             </div>
-            <div className={value !== "" ? "footer": "footer-show"}>
+            <div className={value !== "" ? "footer" : "footer-show"}>
                 <h5>Select your university!</h5>
             </div>
             {/*val===-1?null:<Describe values={country[val]} /> */}
--- src/Components/Describe.js
+++ src/Components/Describe.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Describe.css';
 
 const Describe = (props) => {
-    //console.log(props);
+    const location = useLocation();
+
+    // console.log(location.state.name);
+    // console.log(location.state.web);
+    // console.log(location.state.ct);
+    //console.log(props.location.state);
+
     const waiting = "Information awaited";
     const items = [{
-        stateProvince: null, 
-        name: "Academy of Sports and Physical Training",
-        webPages: ["http://www.aefs.edu.al/"],
-        domains: ["aefs.edu.al"]
+        name: location.state.name,
+        webPages: location.state.web,
+        country: location.state.ct
     }]
-    const webData = items[0].webPages.map((item, index) => <li className='list-items'>{" "}{index +1}{". "}<a href={item}>{item}</a></li>);
+    const webData = items[0].webPages.map((item, index) => <li className='list-items'><a href={item}>{item}</a></li>);
     //const domainData = items.domains.map((item) => <li>{item}</li>);
     return (
         <div className="body-con">
-        <div className='container'>
-            <h3 className='university'><span className='uni'>University Name: </span>{items[0].name !== null ? items[0].name : "Hogwarts"}</h3>
-            <h3 className='state'><span className='uni'>State:</span> {items[0].stateProvince === null ? waiting : items[0].stateProvince} </h3>
-            <h3 className='websites'>More information about the university can be found on: {webData}</h3>
-        </div>
+            <div className='container'>
+                <h3 className='university'><span className='uni'>University Name: </span>{items[0].name !== null ? items[0].name : "Hogwarts"}</h3>
+                <h3 className='state'><span className='uni'>Country:</span> {items[0].country === null ? waiting : items[0].country} </h3>
+                <h3 className='websites'>More information about the university can be found on: </h3>
+                <div className="links">
+                    <h3>{webData}</h3>
+                </div>
+            </div>
         </div>
     )
 }
